a769474fa038de2ec7b88432aeb53a4d15eef57e
James <jegathings@gmail.com>
2021-01-17 15:01:20 -0500

    Got a rough verion of hateoas working in the ui.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const React = require('react'); 
+const ReactDOM = require('react-dom'); 
+const client = require('./client'); 
+
+class App extends React.Component {
+
+  doit = async() =>{
+    let response = await client({method: 'GET', path: 'http://localhost:8080/users'})
+    console.log("Response is")
+    console.log(response.entity._embedded.users)
+  }
+	componentDidMount() { 
+    this.doit()
+		//this.setState({employees: response.entity._embedded.employees});
+	}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>I Love You Dina!</h1>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default App
---
+++ src/client.js
@@ -0,0 +1,47 @@
+const rest = require('rest');
+const defaultRequest = require('rest/interceptor/defaultRequest');
+const mime = require('rest/interceptor/mime');
+const errorCode = require('rest/interceptor/errorCode');
+const baseRegistry = require('rest/mime/registry');
+
+const registry = baseRegistry.child();
+const interceptor = require('rest/interceptor');
+
+registry.register('text/uri-list', function() {
+	/* Convert a single or array of resources into "URI1\nURI2\nURI3..." */
+	return {
+		read: function(str /*, opts */) {
+			return str.split('\n');
+		},
+		write: function(obj /*, opts */) {
+			// If this is an Array, extract the self URI and then join using a newline
+			if (obj instanceof Array) {
+				return obj.map(resource => resource._links.self.href).join('\n');
+			} else { // otherwise, just return the self URI
+				return obj._links.self.href;
+			}
+		}
+	};
+
+});
+registry.register('application/hal+json', require('rest/mime/type/application/hal'));
+
+function doit() {	
+	return interceptor({
+		request: function (request /*, config, meta */) {
+			/* If the URI is a URI Template per RFC 6570 (https://tools.ietf.org/html/rfc6570), trim out the template part */
+			if (request.path.indexOf('{') === -1) {
+				return request;
+			} else {
+				request.path = request.path.split('{')[0];
+				return request;
+			}
+		}
+	});
+
+}
+module.exports = rest
+	.wrap(mime, { registry: registry })
+	.wrap(doit())
+	.wrap(errorCode)
+	.wrap(defaultRequest, { headers: { 'Accept': 'application/hal+json' }});
